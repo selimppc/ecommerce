3c920675239dac21657fbffb64d2ee5fb208a076
mithunadhikary <mithun.cse521@gmail.com>
2017-03-11 23:44:43 -0700

    phone number bigger, clickable, show about us, contact us

--- app/modules/web/Views/layout/web_footer.blade.php
+++ app/modules/web/Views/layout/web_footer.blade.php
@@ -1,62 +1,67 @@
+<div class="col-md-12">
 
-<div id="footer">
-                    <div class="pos-footer-1">
-                        <div class="container">
-                            <div class="row">
-                                <div class="pos-footer-static">
-                                    <div class="footer-static row-fluid">
-                                        <div class="f-col f-col1 span3">
-                                            
-                                            <div class="footer-logo footer-static-content ">
-                                                <a href="#"><img src="{{URL::to('/')}}/web/images/logo.png"></a>                                                
-                                            </div>
-                                        </div>
+                            <div class="footer_container">
 
-                                        <div class="f-col f-col1 span3">
-                                            <div class="footer-static-title title_block">
-                                                <h4>Contact details</h4>
-                                            </div>
-                                            <div class="footer-static-content footer-contactus">
-                                                <br/>
-                                                <p>Tel: 1300 566 662</p>
-                                                <p>Email: info@asimstoys.com.au</p>
-                                            </div>
-                                        </div>
-
-                                        <div class="f-col f-col1 span3">
-                                            <div class="footer-static-title title_block">
-                                                <h4>JOIN OUR NEWSLETTER</h4>
-                                            </div>
-                                            <div class="footer-static-content footer-newsletter">
-                                                <form>
-                                                    <input required type="text" name="email_newsletter" placeholder="Email Address">
-                                                    <input type="submit" name="submit" value="Send">
-                                                </form>
-                                            </div>
-                                        </div>
-
-                                        <div class="f-col f-col1 span3">
-                                            <div class="footer-static-title title_block">
-                                                <h4>Facebook bpage</h4>
-                                            </div>
-                                            <div class="footer-static-content footer-youtube">
-                                                <div class="fb-page" data-href="https://www.facebook.com/Asims-Toys-Kids-Ride-On-Toys-869410779777184/" data-tabs="timeline" data-width="280" data-height="50" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"></div>
-                                            </div>
-                                        </div>
+                                <div class="col-md-3 col-sm-3 col-xs-3 row-left-0">
+                                    <div class="footer_logo">
+                                        <a href="#">
+                                            <img src="{{URL::to('')}}/web/images/footer_logo.png">
+                                        </a>
                                     </div>
                                 </div>
+
+                                <div class="col-md-9 col-sm-9 col-xs-9">
+                                    <ul class="footer_menu">
+                                        <li>
+                                            <a href="https://www.facebook.com/Off-the-wall-framing-PTY-Ltd-1180736021961295/" target="_blank"><img src="{{URL::to('')}}/images/facebook.png"></a>
+                                            <a href="https://www.instagram.com/offthewallframing/" target="_blank"><img src="{{URL::to('')}}/images/instagram.png"></a>
+                                            <a href="https://plus.google.com/u/0/109347086873122701317/about" target="_blank"><img src="{{URL::to('')}}/images/google-plus.png"></a>
+                                        </li>
+                                        <li>
+                                            <a href="{{URL::to('')}}/about-us">About us</a>
+                                        </li>
+
+                                        <li>
+                                            <a href="{{URL::to('')}}/terms-and-conditions">Terms And Conditions</a>
+                                        </li>
+
+                                        <li>
+                                            <a href="{{URL::to('')}}/privacy-security">Privacy & Security</a>
+                                        </li>
+                                        <!-- <li>
+                                            <a href="{{URL::to('')}}/delivery-installation">Delivery & Installation</a>
+                                        </li> -->
+                                       <!--  <li>
+                                            <a href="{{URL::to('')}}/wholesale-customers">Wholesale Customers</a>
+                                        </li>
+                                        <li>
+                                            <a href="{{URL::to('')}}/splashbacks">Splashbacks</a>
+                                        </li> -->
+                                        <li>
+                                            <a href="{{URL::to('')}}/picture-framing-rockdale">Contact us</a>
+                                        </li>
+                                    </ul>
+                                </div>
+
                             </div>
+
+                            <a class="developer_company" href="http://www.visionads.com.au/" rel="nofollow" target="_blank">Seo &amp; Website by VisionsAds</a>
+
                         </div>
+
+
                     </div>
 
-                    <div class="pos-footer-2">
-                        <div class="container">
-                            <a class="developer_company" href="http://www.visionads.com.au/" target="_blank">Seo & Website by VisionsAds</a>
-                        </div>
-                    </div>
                 </div>
-
             </div>
-        </div>
+        </div>  
+
+        <script type="text/javascript">
+        var im_domain = 'visionads';
+        var im_project_id = 8;
+        (function(e,t){window._improvely=[];var n=e.getElementsByTagName("script")[0];var r=e.createElement("script");r.type="text/javascript";r.src="https://"+im_domain+".iljmp.com/improvely.js";r.async=true;n.parentNode.insertBefore(r,n);if(typeof t.init=="undefined"){t.init=function(e,t){window._improvely.push(["init",e,t])};t.goal=function(e){window._improvely.push(["goal",e])};t.conversion=function(e){window._improvely.push(["conversion",e])};t.label=function(e){window._improvely.push(["label",e])}}window.improvely=t;t.init(im_domain,im_project_id)})(document,window.improvely||[])
+        </script>
+
+
     </body>
 </html>
--- app/modules/web/Views/layout/web_master.blade.php
+++ app/modules/web/Views/layout/web_master.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>{{$title}} OFF THE WALL</title>
+        <title>{{$title}}</title>
 
         <link href="{{ URL::asset('web/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" media="all"/>
         <link href="{{ URL::asset('web/css/jquery.fancybox.css') }}" rel="stylesheet" type="text/css" media="all" />
@@ -13,6 +13,21 @@
         <script type='text/javascript' src="{{ URL::asset('web/scripts/jquery.elevatezoom.js') }}"></script> 
         
         <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <meta name="keywords" content="{{@$meta_keywords}}">
+        <meta name="description" content="{{@$meta_description}}">
+
+<script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-83437553-1', 'auto');
+          ga('send', 'pageview');
+
+        </script>
+    </head>
     <body>
 
         <div class="container-fluid">
@@ -30,7 +45,7 @@
                     <div class="middle_container position-relative">
                         <div class="col-md-12 col-sm-12 col-xs-12">
                             <div class="header_container">
-                                <div class="col-md-4 col-sm-4 col-xs-4">
+                                <div class="col-md-4 col-sm-4 col-xs-12">
                                     <div class="row">
                                         <div class="logo_container">
                                             <a href="{{URL::to('')}}">
@@ -39,11 +54,11 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-md-8 col-sm-8">
+                                <div class="col-md-8 col-sm-8 col-xs-12">
                                     <div class="row">
                                         <div class="upper_section">
                                             <div class="hot_line">
-                                                02-95672422
+                                                <a href="tel:02-95672422">02-95672422</a>
                                             </div>
                                             <ul class="top_menu">
                                                                        
@@ -51,7 +66,7 @@
                                                     <a href="{{URL::to('')}}/about-us">About us</a>
                                                 </li>
                                                 <li>
-                                                    <a href="{{URL::to('')}}/contact-us">Contact us</a>
+                                                    <a href="{{URL::to('')}}/picture-framing-rockdale">Contact us</a>
                                                 </li>
                                             </ul>
                                             <div class="my_bag_container">
@@ -96,7 +111,7 @@
 
                             <div class="footer_container">
 
-                                <div class="col-md-3 col-sm-3 col-xs-3 row-left-0">
+                                <div class="col-md-3 col-sm-3 col-xs-12 row-left-0">
                                     <div class="footer_logo">
                                         <a href="#">
                                             <img src="{{URL::to('')}}/web/images/footer_logo.png">
@@ -104,15 +119,14 @@
                                     </div>
                                 </div>
 
-                                <div class="col-md-9 col-sm-9 col-xs-9">
+                                <div class="col-md-9 col-sm-9 col-xs-12">
                                     <ul class="footer_menu">
                                         <li>
                                             <a href="https://www.facebook.com/Off-the-wall-framing-PTY-Ltd-1180736021961295/" target="_blank"><img src="{{URL::to('')}}/images/facebook.png"></a>
-											<a href="https://www.instagram.com/offthewallframing/" target="_blank"><img src="{{URL::to('')}}/images/instagram.png"></a>
-											<a href="https://plus.google.com/u/0/109347086873122701317/about" target="_blank"><img src="{{URL::to('')}}/images/google-plus.png"></a>
+                                            <a href="https://www.instagram.com/offthewallframing/" target="_blank"><img src="{{URL::to('')}}/images/instagram.png"></a>
+                                            <a href="https://plus.google.com/u/0/109347086873122701317/about" target="_blank"><img src="{{URL::to('')}}/images/google-plus.png"></a>
                                         </li>
-										
-										<li>
+                                        <li>
                                             <a href="{{URL::to('')}}/about-us">About us</a>
                                         </li>
 
@@ -133,7 +147,7 @@
                                             <a href="{{URL::to('')}}/splashbacks">Splashbacks</a>
                                         </li> -->
                                         <li>
-                                            <a href="{{URL::to('')}}/contact-us">Contact us</a>
+                                            <a href="{{URL::to('')}}/picture-framing-rockdale">Contact us</a>
                                         </li>
                                     </ul>
                                 </div>
@@ -150,5 +164,15 @@
                 </div>
             </div>
         </div>  
+
+        <script type="text/javascript">
+        var im_domain = 'visionads';
+        var im_project_id = 8;
+        (function(e,t){window._improvely=[];var n=e.getElementsByTagName("script")[0];var r=e.createElement("script");r.type="text/javascript";r.src="https://"+im_domain+".iljmp.com/improvely.js";r.async=true;n.parentNode.insertBefore(r,n);if(typeof t.init=="undefined"){t.init=function(e,t){window._improvely.push(["init",e,t])};t.goal=function(e){window._improvely.push(["goal",e])};t.conversion=function(e){window._improvely.push(["conversion",e])};t.label=function(e){window._improvely.push(["label",e])}}window.improvely=t;t.init(im_domain,im_project_id)})(document,window.improvely||[])
+        </script>
+
+
+    </body>
+</html>
                                 
                             
