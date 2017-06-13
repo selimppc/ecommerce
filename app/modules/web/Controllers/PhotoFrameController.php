d49132dbedb0d7f35a8858df25817da2ffa781a8
mithunadhiakry <mithun.cse521@gmail.com>
2017-06-13 11:13:36 +0600

    Frame Bottom Description

--- app/modules/web/Controllers/PhotoFrameController.php
+++ app/modules/web/Controllers/PhotoFrameController.php
@@ -7,6 +7,7 @@ use App\FrameCategory;
 use App\Mat;
 use App\GlassBacking;
 use App\Printing;
+use App\Article;
 
 class PhotoFrameController extends Controller{
 
@@ -24,13 +25,22 @@ class PhotoFrameController extends Controller{
 
 		$printing_data = Printing::where('status','active')->get();
 
+		$product_description = Article::where('status','active')->where('slug','product-description')->first();
+
+		$how_to_order = Article::where('status','active')->where('slug','how-to-order')->first();
+
+		$shipping_rule = Article::where('status','active')->where('slug','shipping-returns')->first();
+
         return view('web::photo_frame.main',[
                 'title' => $title,
                 'data'  => $data,
                 'frame_category' => $frame_category,
                 'mat_data' => $mat_data,
                 'glass_backing_data' => $glass_backing_data,
-                'printing_data' => $printing_data
+                'printing_data' => $printing_data,
+                'product_description' => $product_description,
+                'how_to_order' => $how_to_order,
+                'shipping_rule' => $shipping_rule
             ]);
 
 	}
