3fed551e5f9756574bde1d0f7f54ea43fb9fa75c
mithunadhiakry <mithun.cse521@gmail.com>
2017-06-27 19:44:42 +0600

    color changes

--- app/modules/web/Views/photo_frame/bottom_description.blade.php
+++ app/modules/web/Views/photo_frame/bottom_description.blade.php
@@ -1,25 +1,25 @@
-<div class="container-fluid col-xs-10 col-xs-push-1" id="text-content-container">
+<div class="container-fluid col-xs-10 col-xs-push-1 bottom-description" id="text-content-container">
 	<ul class="nav nav-tabs" role="tablist">
 		<li role="presentation" class="active"><a href="#product-description-tab" aria-controls="product-description-tab" role="tab" data-toggle="tab">Product Description</a></li>
 		<li role="presentation"><a href="#faq-tab" aria-controls="faq-tab" role="tab" data-toggle="tab">How to Order</a></li>
 		<li role="presentation"><a href="#shipping-tab" aria-controls="shipping-tab" role="tab" data-toggle="tab">Shipping & Returns</a></li>
 	</ul>
 	<div class="tab-content">
-		<div role="tabpanel" class="tab-pane fade in active" id="product-description-tab">
+		<div style="color: #fff;" role="tabpanel" class="tab-pane fade in active" id="product-description-tab">
 			
 			@if(!empty($product_description))
 				{!! $product_description->desc !!}
 			@endif
 			
 		</div>
-		<div role="tabpanel" class="tab-pane fade" id="faq-tab">
+		<div style="color: #fff;" role="tabpanel" class="tab-pane fade" id="faq-tab">
 			
 			@if(!empty($how_to_order))
 				{!! $how_to_order->desc !!}
 			@endif
 
 		</div>
-		<div role="tabpanel" class="tab-pane fade" id="shipping-tab">
+		<div style="color: #fff;" role="tabpanel" class="tab-pane fade" id="shipping-tab">
 			
 			@if(!empty($shipping_rule))
 				{!! $shipping_rule->desc !!}
@@ -28,3 +28,4 @@
 		</div>
 	</div>
 </div>
+
